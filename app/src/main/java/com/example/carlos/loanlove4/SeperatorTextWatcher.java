c1bd1778871d52be02a58e795c1059c32639dcfa
Carlos <carlos42044@gmail.com>
2017-08-23 13:46:48 -0400

    working on optimizations

---
+++ app/src/main/java/com/example/carlos/loanlove4/SeperatorTextWatcher.java
@@ -0,0 +1,26 @@
+package com.example.carlos.loanlove4;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * Created by CarlosF on 8/22/2017.
+ */
+
+public class SeperatorTextWatcher implements TextWatcher{
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+}
